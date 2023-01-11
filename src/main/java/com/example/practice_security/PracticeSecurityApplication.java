f97953680ae547a5dc51004fa36f9f0ebb4ab4e1
yymasaki <y.masaki34@icloud.com>
2023-01-12 08:55:14 +0900

    securityの実装

---
+++ src/main/java/com/example/practice_security/PracticeSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSecurityApplication.class, args);
+	}
+
+}
